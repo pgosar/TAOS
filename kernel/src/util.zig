a0c36550735259d9ade81b39edfc0050f2ec4c2e
Pranay Gosar <pgosarsteam@gmail.com>
2025-01-29 12:48:43 -0600

    Add a debug print mode (#8)
    * add debug print

---
+++ kernel/src/util.zig
@@ -0,0 +1,8 @@
+const builtin = @import("builtin");
+const serial = @import("./drivers/serial.zig");
+
+pub inline fn debugPrint(comptime format: []const u8, args: anytype) void {
+    if (builtin.mode == .Debug) {
+        serial.println(format, args);
+    }
+}
