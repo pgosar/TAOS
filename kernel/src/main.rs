499717d10b506333500227fe88ce372c95b536c5
Amerikranian <olegjpittman@gmail.com>
2025-01-29 19:16:05 -0600

    Kernel: Add smp boot

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
-use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
+use core::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker, SmpRequest};
+use limine::smp::Cpu;
 use limine::BaseRevision;
-use x86_64::instructions::hlt;
 use taos::serial_println;
+use x86_64::instructions::{hlt, interrupts};
 
 #[used]
 #[link_section = ".requests"]
@@ -15,6 +17,10 @@ static BASE_REVISION: BaseRevision = BaseRevision::new();
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
 #[used]
+#[link_section = ".requests"]
+static SMP_REQUEST: SmpRequest = SmpRequest::new();
+
+#[used]
 #[link_section = ".requests_start_marker"]
 static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
 
@@ -22,13 +28,17 @@ static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
 #[link_section = ".requests_end_marker"]
 static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
 
+static BOOT_COMPLETE: AtomicBool = AtomicBool::new(false);
+static CPU_COUNT: AtomicU64 = AtomicU64::new(0);
+
 #[no_mangle]
 extern "C" fn kmain() -> ! {
     assert!(BASE_REVISION.is_supported());
 
-    serial_println!("Booting...");
+    serial_println!("Booting on BSP...");
+
     if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
-        taos::serial_println!("Found frame buffer");
+        serial_println!("Found frame buffer");
         if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
             for i in 0..100_u64 {
                 let pixel_offset = i * framebuffer.pitch() + i * 4;
@@ -39,17 +49,51 @@ extern "C" fn kmain() -> ! {
         }
     }
 
-    taos::serial_println!("Halting");
+    let smp_response = SMP_REQUEST.get_response().expect("SMP request failed");
+    let cpu_count = smp_response.cpus().len() as u64;
+
+    serial_println!("Detected {} CPU cores", cpu_count);
+
+    let bsp_id = smp_response.bsp_lapic_id();
+    for cpu in smp_response.cpus() {
+        if cpu.id != bsp_id {
+            cpu.goto_address.write(secondary_cpu_main);
+        }
+    }
+
+    while CPU_COUNT.load(Ordering::SeqCst) < cpu_count - 1 {
+        core::hint::spin_loop();
+    }
+
+    BOOT_COMPLETE.store(true, Ordering::SeqCst);
+    serial_println!("All CPUs initialized");
+
+    serial_println!("BSP entering idle loop");
+    hcf();
+}
+
+#[no_mangle]
+unsafe extern "C" fn secondary_cpu_main(cpu: &Cpu) -> ! {
+    CPU_COUNT.fetch_add(1, Ordering::SeqCst);
+    serial_println!("AP {} initialized", cpu.id);
+
+    while !BOOT_COMPLETE.load(Ordering::SeqCst) {
+        core::hint::spin_loop();
+    }
+
+    serial_println!("AP {} entering idle loop", cpu.id);
     hcf();
 }
 
 #[panic_handler]
-fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
+fn rust_panic(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("Kernel panic: {}", info);
     hcf();
 }
 
 fn hcf() -> ! {
     loop {
+        interrupts::disable();
         hlt();
     }
 }
