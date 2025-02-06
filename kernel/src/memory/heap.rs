a565d989451af120cbe6aadcecb8bcc828e5ab53
Kshitij Kapoor <35787705+KshitijKapoor8@users.noreply.github.com>
2025-02-06 11:22:47 -0600

    "Smart" frame allocator (#13)
    * created dynamically allocated bitmap that should support freeing
    
    * code cleanup, testing, and free_frames count
    
    * global frame_alloc enum made
    
    * added global pmm reference and mutex, changed bitmap allocation creation and heap initialization logic
    
    * use div ceil
    
    * cleanup
    
    ---------
    
    Co-authored-by: pgosar <pgosarsteam@gmail.com>

---
+++ kernel/src/memory/heap.rs
@@ -0,0 +1,58 @@
+extern crate alloc;
+
+use crate::constants::memory::{HEAP_SIZE, HEAP_START};
+use crate::memory::{frame_allocator::FRAME_ALLOCATOR, paging::create_mapping};
+use crate::serial_println;
+use talc::{ClaimOnOom, Span, Talc, Talck};
+use x86_64::{
+    structures::paging::{mapper::MapToError, Mapper, Page, Size4KiB},
+    VirtAddr,
+};
+
+use super::bitmap_frame_allocator::BitmapFrameAllocator;
+use super::frame_allocator::GlobalFrameAllocator;
+
+#[global_allocator]
+static ALLOCATOR: Talck<spin::Mutex<()>, ClaimOnOom> = Talc::new(unsafe {
+    ClaimOnOom::new(Span::new(HEAP_START, HEAP_START.wrapping_add(HEAP_SIZE)))
+})
+.lock();
+
+/// Initialize the heap and switch to using the bitmap frame_allocator
+pub fn init_heap(mapper: &mut impl Mapper<Size4KiB>) -> Result<(), MapToError<Size4KiB>> {
+    let page_range = {
+        let heap_start = VirtAddr::new(HEAP_START as u64);
+        let heap_end = heap_start + HEAP_SIZE as u64 - 1u64;
+        let heap_start_page = Page::containing_address(heap_start);
+        let heap_end_page = Page::containing_address(heap_end);
+        Page::range_inclusive(heap_start_page, heap_end_page)
+    };
+
+    for page in page_range {
+        create_mapping(page, mapper);
+    }
+
+    switch_allocator();
+
+    serial_println!("Allocator switched to bitmap allocator");
+
+    Ok(())
+}
+
+fn switch_allocator() {
+    let mut alloc = FRAME_ALLOCATOR.lock();
+    match *alloc {
+        Some(GlobalFrameAllocator::Boot(ref boot_alloc)) => {
+            unsafe {
+                let bitmap_frame_allocator = BitmapFrameAllocator::init(
+                    boot_alloc.memory_map,
+                    boot_alloc.allocated_frames(),
+                );
+                *alloc = Some(GlobalFrameAllocator::Bitmap(bitmap_frame_allocator));
+
+                serial_println!("new frame allocator set");
+            };
+        }
+        _ => panic!("We must be using Boot Frame Allocator at this point"),
+    }
+}
