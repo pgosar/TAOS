a5c60a73bba70dfdc2843152e5163daa2bb1f2c0
Joseph Wilson <josephcwilson47@gmail.com>
2025-02-11 13:39:07 -0600

    Formatted

--- kernel/src/events/event.rs
+++ kernel/src/events/event.rs
@@ -2,8 +2,8 @@ extern crate alloc;
 use alloc::boxed::Box;
 use alloc::sync::Arc;
 
-use core::usize;
 use core::future::Future;
+use core::usize;
 
 use spin::Mutex;
 
@@ -12,22 +12,28 @@ use futures::task::ArcWake;
 use super::{Event, EventId, EventQueue};
 
 impl Event {
-  pub fn init(future: impl Future<Output = ()> + 'static + Send, rewake_queue: EventQueue, priority: usize) -> Event {
-    Event {
-        eid: EventId::init(),
-        future: Mutex::new(Box::pin(future)),
-        rewake_queue,
-        priority
+    pub fn init(
+        future: impl Future<Output = ()> + 'static + Send,
+        rewake_queue: EventQueue,
+        priority: usize,
+    ) -> Event {
+        Event {
+            eid: EventId::init(),
+            future: Mutex::new(Box::pin(future)),
+            rewake_queue,
+            priority,
+        }
     }
-  }
 }
 
 impl ArcWake for Event {
     fn wake_by_ref(arc: &Arc<Self>) {
-      let r: Result<(), Arc<Event>> = arc.rewake_queue.push(arc.clone());
-      match r {
-        Err(_) => {panic!("Event queue full!")}
-        Ok(_) => {},
-      }
+        let r: Result<(), Arc<Event>> = arc.rewake_queue.push(arc.clone());
+        match r {
+            Err(_) => {
+                panic!("Event queue full!")
+            }
+            Ok(_) => {}
+        }
     }
 }
